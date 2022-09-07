d2f2b784753a50c9d6e237b1baa3798f678e2f31
MANURI <manuri@deepdatainsight.com>
2022-09-07 12:57:08 +0530

    updated

--- run.py
+++ run.py
@@ -10,6 +10,8 @@ print("added from remote")
 print("want to add this line")
 
 print("new version below")
+print("check with this line")
 
+print("check merge again")
 print("want to keep this line")
 print("Number to print 3")
